94cc1a698fb19078369b9517cd8e6dc573cd1288
malev <marcos.vanetta@nytimes.com>
2024-12-22 00:31:17 -0600

    Version v0.0.1

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Hola",
 	Long:  `All software has versions. This is Hola's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hola HTTP Client v0.9 -- HEAD")
+		fmt.Println("Hola HTTP Client v0.0.1")
 	},
 }
