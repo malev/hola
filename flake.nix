6afc4a136e6bb8023567dcae25a226ba57961a44
malev <marcosvanetta@gmail.com>
2025-02-02 14:44:49 -0600

    Adding support to multi systems

--- flake.nix
+++ flake.nix
@@ -2,21 +2,20 @@
   description = "hola is an HTTP client cli application";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Or a specific commit
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.hola = pkgs.buildGoModule {
-        pname = "hola";
-        version = "0.0.1";
-        src = ./.;
-        vendorHash = "sha256-hocnLCzWN8srQcO3BMNkd2lt0m54Qe7sqAhUxVZlz1k==";
-        subPackages = [ "." ];
-      };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.hola = pkgs.buildGoModule {
+          pname = "hola";
+          version = "0.0.1";
+          src = ./.;
+          vendorHash = "sha256-hocnLCzWN8srQcO3BMNkd2lt0m54Qe7sqAhUxVZlz1k==";
+          subPackages = [ "." ];
 
-      defaultPackage.${system} = self.packages.${system}.hola;
-    };
+        };
+        defaultPackage = self.packages.${system}.hola;
+      });
 }
